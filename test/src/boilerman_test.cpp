aeb91d2bf5928596d48526250ff159a4e025adf6
Sally <spoovathumkadavil16@claytonschools.net>
2024-09-02 14:54:47 -0500

    lib upd

--- test/src/boilerman_test.cpp
+++ test/src/boilerman_test.cpp
@@ -4,13 +4,13 @@
 TEST(LibTests, TestNameIsRight_Zero)
 {
   auto const lib = library {};
-  ASSERT_EQ(lib.name, "boilerman");
+  ASSERT_EQ(lib._name, "boilerman");
 }
 
 TEST(LibTests, TestNameIsRight_One)
 {
   auto const lib = library {};
-  ASSERT_NE(lib.name, "not boilerman");
+  ASSERT_NE(lib._name, "not boilerman");
 }
 
 int main(int argc, char** argv)
