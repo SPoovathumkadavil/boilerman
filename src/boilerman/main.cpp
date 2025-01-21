7f005f88e0d56e2dd8beddc9d67af6ca6a54aa95
sally <spoovathumkadavil16@claytonschools.net>
2025-01-20 18:19:10 -0600

    port boilerman to fetalib v0.2

--- src/boilerman/main.cpp
+++ src/boilerman/main.cpp
@@ -2,96 +2,135 @@
 #include "main.hpp"
 
 #include <fetalib/cli/colors.hpp>
+#include <fetalib/cli/arguments.hpp>
 
 #include "boiler.hpp"
 
 int main(int argc, char** argv)
 {
+
+  feta::ArgumentParser argparser(argc, argv);
+  
+  feta::ArgumentDependency c_help = feta::get_blank_argument_dependency()
+    .withKey("help")
+    .withHelpMessage(
+      "Display the help message."
+    );
+  feta::ArgumentDependency c_init = feta::get_blank_argument_dependency()
+    .withKey("init")
+    .withHelpMessage(
+      "Initialize a boilerplate."
+    );
+  feta::ArgumentDependency c_list = feta::get_blank_argument_dependency()
+    .withKey("list")
+    .withHelpMessage("List available boilerplates.");
+  feta::Argument a_index = feta::get_blank_argument()
+    .withKey("-i")
+    .withAlternateKey("--index")
+    .withOptional()
+    .withWordCount(1)
+    .withHelpMessage("Index of the boilerplate you are trying to initialize.");
+  feta::Argument a_project_name = feta::get_blank_argument()
+    .withKey("-n")
+    .withAlternateKey("--name")
+    .withOptional()
+    .withWordCount(1)
+    .withHelpMessage("Your project's name.");
+  feta::Argument a_dir = feta::get_blank_argument()
+    .withKey("-d")
+    .withAlternateKey("--directory")
+    .withOptional()
+    .withWordCount(1)
+    .withHelpMessage("Your project's directory.");
+  argparser.add(&c_help);
+  argparser.add(&c_init);
+  argparser.add(&a_index, &c_init);
+  argparser.add(&a_project_name, &c_init);
+  argparser.add(&a_dir, &c_init);
+  argparser.add(&c_list);
+
+  // -- validation --
+
+  feta::Validation argument_validation = argparser.validate();
+  if (!argument_validation.valid) {
+    std::printf("Argument Error: %s\n", argument_validation.detail.c_str());
+    exit(1);
+  }
+
+  // -- init --
+
   auto lib = library {};
   auto boilers = lib.populate_boilers();
 
-  if (argc < 2) {
-    std::printf(feta::colorize("error: no arguments provided.\ntry %s --help for usage instructions.\n", feta::Color::RED).c_str(), lib._name.c_str());
-    return 1;
-  }
+  // -- command parsing --
 
-  std::string project_name = "project";
-  std::filesystem::path dir = ".";
-  int b_index = -1;
+  feta::ArgumentDependency *command = argparser.get_command();
 
-  for (int i = 0; i < argc; i++) {
-    if (std::string(argv[i]) == "--help" || std::string(argv[i]) == "-h") {
-      std::printf(feta::colorize("usage: %s [options] [command]\n", feta::Color::BLUE).c_str(), lib._name.c_str());
-      std::printf(feta::colorize("options:\n", feta::Color::BLUE).c_str());
-      std::printf(feta::colorize("\t-n, --name [name]\t\tset name.\n", feta::Color::BLUE).c_str());
-      std::printf(feta::colorize("commands:\n", feta::Color::BLUE).c_str());
-      std::printf(feta::colorize("\ti, init [index]\t\tinitialize a boilerplate.\n", feta::Color::BLUE).c_str());
-      std::printf(feta::colorize("\ta, add [dir]\t\tadd a boilerplate.\n", feta::Color::BLUE).c_str());
-      std::printf(feta::colorize("\t-h, --help\t\tshow this help message.\n", feta::Color::BLUE).c_str());
-      std::printf(feta::colorize("\t-l, --list\t\tlist available boilerplates.\n", feta::Color::BLUE).c_str());
-      return 0;
-    }
-
-    if (std::string(argv[i]) == "--list" || std::string(argv[i]) == "-l") {
-      for (int b = 0; b < (int)boilers.size(); b++) {
-        std::printf(feta::colorize("%i - %s: %s (%s)\n", feta::Color::BLUE).c_str(), b + 1,
-                   boilers[b]._lang.c_str(),
-                   boilers[b]._desc.c_str(),
-                   boilers[b]._type.c_str());
-      }
-      return 0;
-    }
-
-    if (std::string(argv[i]) == "init" || std::string(argv[i]) == "i") {
-      if (i + 1 >= argc) {
-        std::printf(feta::colorize("available boilerplates:\n", feta::Color::BLUE).c_str());
-        for (int b = 0; b < (int)boilers.size(); b++) {
-          std::printf(feta::colorize("%i - %s: %s (%s)\n", feta::Color::BLUE).c_str(), b + 1,
-                      boilers[b]._lang.c_str(),
-                      boilers[b]._desc.c_str(),
-                      boilers[b]._type.c_str());
-        }
-
-        // read boiler index
-        int u_ind = -1;
-        while (u_ind < 0) {
-          std::printf(feta::colorize("enter index %i-%i: ", feta::Color::BLUE).c_str(), 1, (int)boilers.size());
-          std::cin >> u_ind;
-          if (u_ind < 1 || u_ind > (int)boilers.size())
-            u_ind = -1;
-        }
-        b_index = u_ind - 1;
-
-        // read project name
-        std::printf(feta::colorize("project name: ", feta::Color::BLUE).c_str());
-        std::cin >> project_name;
-        // read boiler destination directory
-        std::printf(feta::colorize("destination directory: ", feta::Color::BLUE).c_str());
-        std::cin >> dir;
-      } else {
-        b_index = std::stoi(argv[i + 1]) - 1;
-
-        if (i + 2 < argc)
-          dir = std::string(argv[i + 2]);
-      }
-    }
-
-    if (std::string(argv[i]) == "--name" || std::string(argv[i]) == "-n") {
-      if (i + 1 >= argc) {
-        std::printf(feta::colorize("error: project name not provided.\ntry %s --help for usage instructions.\n", feta::Color::RED).c_str(), lib._name.c_str());
-        return 1;
-      }
-
-      project_name = std::string(argv[i + 1]);
+  if (command == &c_list) {
+    for (int b = 0; b < (int)boilers.size(); b++) {
+      std::printf(feta::colorize("%i - %s: %s (%s)\n", feta::Color::BLUE).c_str(), b + 1,
+                  boilers[b]._lang.c_str(),
+                  boilers[b]._desc.c_str(),
+                  boilers[b]._type.c_str());
     }
   }
 
-  // construct resolve map
-  std::map<std::string, std::string> resolve_map;
-  resolve_map["name"] = project_name;
+  if (command == &c_help) {
+    std::vector<std::string> hs = argparser.get_help_message(lib._name);
+    for (std::string s : hs) std::printf("%s\n", s.c_str());
+  }
 
-  if (b_index >= 0) {
-    boilers[b_index].use(dir, resolve_map);
+  if (command == &c_init) {
+    int index = -1;
+    std::string project_name = "";
+    std::string dir = "";
+    int q = 0;
+    if (argparser.get<std::string>(a_index).has_value()) {
+      index = argparser.get<int>(a_index).value()-1;
+    }
+    if (argparser.get<std::string>(a_project_name).has_value()) {
+      project_name = argparser.get<std::string>(a_project_name).value();
+    }
+    if (argparser.get<std::string>(a_dir).has_value()) {
+      dir = argparser.get<std::string>(a_dir).value();
+    }
+
+    if (index < 0 || index > boilers.size()) {
+      std::printf(feta::colorize("available boilerplates:\n", feta::Color::BLUE).c_str());
+      for (int b = 0; b < boilers.size(); b++) {
+        std::printf(feta::colorize("%i - %s: %s (%s)\n", feta::Color::BLUE).c_str(),
+                    b + 1,
+                    boilers[b]._lang.c_str(),
+                    boilers[b]._desc.c_str(),
+                    boilers[b]._type.c_str());
+      }
+
+      int u_ind = -1;
+      while (u_ind < 0) {
+        std::printf(feta::colorize("enter index %i-%i: ", feta::Color::BLUE).c_str(), 1, (int)boilers.size());
+        std::cin >> u_ind;
+        if (u_ind < 1 || u_ind > boilers.size())
+          u_ind = -1;
+      }
+      index = u_ind - 1;
+    }
+
+    if (project_name == "") {
+      // read project name
+      std::printf(feta::colorize("project name: ", feta::Color::BLUE).c_str());
+      std::cin >> project_name;
+    }
+
+    if (dir == "") {
+      // read boiler destination directory
+      std::printf(feta::colorize("destination directory: ", feta::Color::BLUE).c_str());
+      std::cin >> dir;
+    }
+
+    // construct resolve map
+    std::map<std::string, std::string> resolve_map;
+    resolve_map["name"] = project_name;
+    boilers[index].use(dir, resolve_map);
   }
 
   return 0;
